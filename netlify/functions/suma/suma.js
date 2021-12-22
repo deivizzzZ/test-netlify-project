78b8e38768144e27e7c1124b61e15875022392b9
David Alvarez <marava.jr@gmail.com>
2021-12-22 18:45:34 +0100

    commit

--- netlify/functions/suma/suma.js
+++ netlify/functions/suma/suma.js
@@ -1,11 +1,13 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const suma = (a, b) => a + b
+
 const handler = async (event) => {
   try {
     const number1 = parseInt(event.queryStringParameters.num1) || 1
     const number2 = parseInt(event.queryStringParameters.num2) || 2
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `${number1} + ${number2} = ${number1 + number2}` }),
+      body: JSON.stringify({ message: `${number1} + ${number2} = ${suma(number1, number2)}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
